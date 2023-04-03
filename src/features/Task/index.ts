92ec5ddead464f40a6129f9478f627b586709719
ShotMeow <shotmeow@gmail.com>
2023-04-03 17:05:11 +0300

    Fixed: Delete Unused Code

--- src/features/Task/index.ts
+++ src/features/Task/index.ts
@@ -1,5 +1,4 @@
 export { AddTaskModal } from './ui/AddTaskModal';
-export { getTasks } from './selectors';
 export { TaskCard } from './ui/TaskCard';
 export { taskApi } from './queries';
 export { type TaskType } from './types';
--- src/features/Task/selectors/index.ts
+++
@@ -1,4 +0,0 @@
-import { type RootState } from '@/app/store';
-import { type TaskType } from '@/features/Task/types';
-
-export const getTasks = (state: RootState): TaskType[] | null => state.task.tasks;
