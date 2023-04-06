822eefa919307c688a7bc8ab9fe40d9df77e2128
ShotMeow <shotmeow@gmail.com>
2023-04-06 15:02:14 +0300

    Fixed: Small Visual Fixes

--- src/features/Board/ui/BoardSelectDropdown/index.tsx
+++ src/features/Board/ui/BoardSelectDropdown/index.tsx
@@ -1,13 +1,14 @@
-import React, { type FC } from 'react';
+import React, { type FC, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { Dropdown } from '@/shared/ui';
-import { getBoards, getCurrentBoard, setCurrentBoard } from '@/features/Board';
+import { CreateBoardModal, getBoards, getCurrentBoard, setCurrentBoard } from '@/features/Board';
 
 import { type BoardType } from '../../types';
 
 import styles from './BoardSelectDropdown.module.scss';
 import classNames from 'classnames';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface Props {
   setDropdownShown: React.Dispatch<React.SetStateAction<boolean>>;
@@ -15,6 +16,7 @@ interface Props {
 }
 
 export const BoardSelectDropdown: FC<Props> = ({ setDropdownShown, dropdownShown }) => {
+  const [createBoardModalShown, setCreateBoardModalShown] = useState<boolean>(false);
   const boards = useSelector(getBoards);
   const currentBoard = useSelector(getCurrentBoard);
   const dispatch = useDispatch();
@@ -25,23 +27,46 @@ export const BoardSelectDropdown: FC<Props> = ({ setDropdownShown, dropdownShown
   };
 
   return (
-    <Dropdown className={styles.dropdown} onShownChange={setDropdownShown} shown={dropdownShown}>
-      <ul>
-        {boards?.map((board) => (
-          <li key={board.id}>
-            <button
-              className={classNames({
-                [styles.active]: board.id === currentBoard?.id,
-              })}
-              onClick={() => {
-                handleSelect(board);
+    <>
+      <Dropdown className={styles.dropdown} onShownChange={setDropdownShown} shown={dropdownShown}>
+        <ul>
+          {boards?.map((board) => (
+            <li key={board.id}>
+              <motion.button
+                whileTap={{ scale: 0.95 }}
+                className={classNames({
+                  [styles.active]: board.id === currentBoard?.id,
+                })}
+                onClick={() => {
+                  handleSelect(board);
+                }}
+              >
+                {board.title}
+              </motion.button>
+            </li>
+          ))}
+          <li>
+            <motion.button
+              className={styles.active}
+              whileTap={{ scale: 0.95 }}
+              onClick={(event) => {
+                event.stopPropagation();
+                setCreateBoardModalShown(true);
               }}
             >
-              {board.title}
-            </button>
+              <span>+ Create New Board</span>
+            </motion.button>
           </li>
-        ))}
-      </ul>
-    </Dropdown>
+        </ul>
+      </Dropdown>
+      <AnimatePresence>
+        {createBoardModalShown && (
+          <CreateBoardModal
+            createBoardModalShown={createBoardModalShown}
+            setCreateBoardModalShown={setCreateBoardModalShown}
+          />
+        )}
+      </AnimatePresence>
+    </>
   );
 };
