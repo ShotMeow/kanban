7e8328ffd6870cfe092e53b5caeb996e998c67e5
ShotMeow <shotmeow@gmail.com>
2023-04-14 20:53:24 +0300

    Fixed: Small Visual & SEO Bugs

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,9 +4,5 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from '@/shared/libs/react-router-dom';
 
 export const App: FC = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
