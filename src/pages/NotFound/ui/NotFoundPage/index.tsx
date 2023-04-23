31b369e2933e571e542f6d152c6f3975ce56ee7e
ShotMeow <shotmeow@gmail.com>
2023-04-23 16:38:00 +0300

    Change: 404 Not Found Page

--- src/pages/NotFound/ui/NotFoundPage/index.tsx
+++ src/pages/NotFound/ui/NotFoundPage/index.tsx
@@ -1,15 +1,31 @@
 import React, { type FC } from 'react';
 
-import { Logo } from '@/shared/ui';
+import { Button } from '@/shared/ui';
 
 import styles from './NotFoundPage.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFoundPage: FC = () => {
+  const navigate = useNavigate();
+
   return (
     <main className={styles.notfound}>
-      <Logo />
       <div>
-        <h1>Page Not Found</h1>
+        <div className={styles.code}>
+          <p>404 error</p>
+        </div>
+        <div className={styles.text}>
+          <h1>We&apos;ve lost this page</h1>
+          <p>Sorry, the page you are looking for doesn&apos;t exist or has been moved.</p>
+        </div>
+        <Button
+          onClick={() => {
+            navigate('/');
+          }}
+          primary
+        >
+          Go Back
+        </Button>
       </div>
     </main>
   );
