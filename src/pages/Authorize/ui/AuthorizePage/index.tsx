a4a194e75097fa43e0fe185030d9e4484aaafde7
ShotMeow <shotmeow@gmail.com>
2023-04-23 15:32:45 +0300

    Added: AuthorizePage Animation to Image

--- src/pages/Authorize/ui/AuthorizePage/index.tsx
+++ src/pages/Authorize/ui/AuthorizePage/index.tsx
@@ -1,5 +1,5 @@
 import React, { type FC, useState } from 'react';
-
+import { motion } from 'framer-motion';
 import { SignIn, SignUp } from '@/features/Authorize';
 
 import kanbanImageSrc from '@/shared/images/kanban_todo_app.jpg';
@@ -8,6 +8,7 @@ import styles from './AuthorizePage.module.scss';
 
 export const AuthorizePage: FC = () => {
   const [isSignIn, setIsSignIn] = useState<boolean>(true);
+
   return (
     <main className={styles.authorize}>
       <div className={styles.actions}>
@@ -18,7 +19,21 @@ export const AuthorizePage: FC = () => {
           <h1>Kanban Todo Application</h1>
           <p>Keeping a list of tasks has become even easier</p>
         </div>
-        <img src={kanbanImageSrc} width={1920} height={1080} alt="Kanban Application Preview" />
+        <motion.img
+          whileHover={{ scale: 1.01 }}
+          drag
+          dragConstraints={{
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+          transition={{ type: 'spring', stiffness: 100 }}
+          src={kanbanImageSrc}
+          width={1920}
+          height={1080}
+          alt="Kanban Application Preview"
+        />
       </div>
     </main>
   );
